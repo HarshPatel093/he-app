f38a36ba26248582d2e08182853e29d2e8a8d3a4
HarshPatel093 <harshpatel.adl@gmail.com>
2025-08-20 08:12:06 +0930

    update super user settings
    updated superuser setting, so that other teammate can use same database.

--- create_superuser_if_not_exists.py
+++ create_superuser_if_not_exists.py
@@ -1,22 +1,22 @@
 import os
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-django.setup()
+def run():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+    django.setup()
 
-from django.contrib.auth import get_user_model
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
 
-User = get_user_model()
+    username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+    email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
-username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
-password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-
-if username and email and password:
-    if not User.objects.filter(username=username).exists():
-        print(f"Creating superuser {username}...")
-        User.objects.create_superuser(username=username, email=email, password=password)
+    if username and email and password:
+        if not User.objects.filter(username=username).exists():
+            print(f"Creating superuser {username}...")
+            User.objects.create_superuser(username=username, email=email, password=password)
+        else:
+            print(f"Superuser {username} already exists.")
     else:
-        print(f"Superuser {username} already exists.")
-else:
-    print("Missing environment variables for superuser.")
+        print("Missing environment variables for superuser.")
