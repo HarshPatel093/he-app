7d5a9ea89a5cd0b1bb1ef1a5ef64b1f7d8ca9822
HarshPatel093 <harshpatel.adl@gmail.com>
2025-08-20 18:17:56 +0930

    database changes

--- core/settings.py
+++ core/settings.py
@@ -57,12 +57,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'he-app',                
-        'USER': 'HolidayExplorers',      
-        'PASSWORD': '1212',
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
