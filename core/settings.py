5bd2955769f2caee238002ae8e4f310e3ef07dd1
HarshPatel093 <harshpatel.adl@gmail.com>
2025-08-23 14:32:48 +0930

    create frontend for admin dashboard
    Implemented Admin Dashboard UI with Nunito font and responsive styling
    
    Added navigation bar with logo, menu items like Home, Staff, Clients, Create User and profile
    
    Integrated static images (logo.jpg, profile.svg) with Django static files

--- core/settings.py
+++ core/settings.py
@@ -81,6 +81,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if os.environ.get('RUN_MAIN') == 'true':  # prevents running twice with autoreload
