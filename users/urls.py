385b5c8e9f5c4bb137c1aacefa5f24c983ad5a34
Neha Kondabathini <konny016@mymail.unisa.edu.au>
2025-09-08 13:47:01 +0930

    fixed urls

--- users/urls.py
+++ users/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('dashboard/client/feedback', views.client_feedback, name = 'client_feedback'),
 
+
     path("clients-list/", views.clients_list, name="clients_list"),
 
     path("client/<int:client_id>/", views.client_detail, name="client_detail"),
@@ -24,6 +25,8 @@ urlpatterns = [
 
     
     
+
+   
 
 ]
 
