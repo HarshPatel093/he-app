428315338f3e4673fd8c9d917dd8b682f6c0d97b
Neha Kondabathini <konny016@mymail.unisa.edu.au>
2025-10-21 23:55:36 +1030

    removing the sort button and having a drop down instead to look at graphs in monthly basis

--- users/models.py
+++ users/models.py
@@ -31,10 +31,6 @@ class Feedback(models.Model):
     comment = models.TextField(blank=True, null=True)
     photo = models.ImageField(upload_to="feedback_photos/", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    is_staff_feedback = models.BooleanField(default=False)
-    
-    def __str__(self):
-        return f"{self.user.email} - {'Staff' if self.is_staff_feedback else 'Client'} Feedback"
 
 class GoalType(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -45,7 +41,7 @@ class GoalType(models.Model):
 class Goal(models.Model): 
     client = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="goals")
     goal_type = models.ForeignKey(GoalType, on_delete=models.CASCADE, null=True, blank=True)
-    progress = models.IntegerField(default=0) 
+    progress = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
